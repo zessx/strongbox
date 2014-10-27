b62c3a1c0b97545688972fa255af69e6f1a2f348
zessx <contact@zes.sx>
2014-10-27 18:08:56 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    document.body.className = 'question';
+}
+function generate() {
+    var urlparts = window.location.pathname.substr(1).split('/');
+    var startto  = 0;
+    var service  = urlparts[startto] || '';
+    var length   = urlparts[startto+1] || 12;
+    var password = document.getElementById('password').value + length;
+    document.getElementById('password').value = null;
+
+    document.body.className = 'answer';
+    document.getElementById('answer').innerHTML = btoa(CryptoJS.SHA256(service + password).toString()).slice(0, length);
+
+    return false;
+}
